17336a03946131cbd99e4e02b774aede4db693a8
inicio <inicio@FISEP09>
2024-04-29 09:01:07 -0600

    Agregar archivos de proyecto.

---
+++ EjemploCarpeta/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EjemploCarpeta
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Directory.CreateDirectory("temporal");
+            //para generar una carpeta en una ruta especifica
+            //se usa @"ubicacion"
+            DirectoryInfo dir = new DirectoryInfo(@"C:\tempora 2");
+            dir.Create();
+            MessageBox.Show("se creo la carpeta");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Directory.Move("temporal", @"C:\temporal");
+            MessageBox.Show("se movio la carpeta");
+        }
+    }
+}
